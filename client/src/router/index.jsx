aa136b5225a0275d8515f98a8c16ca7a1405ffda
fauzandwip <fauzan27dp@gmail.com>
2023-11-13 22:09:11 +0700

    feat: create react router

--- client/src/router/index.jsx
+++ client/src/router/index.jsx
@@ -1,2 +1,21 @@
+import { createBrowserRouter } from 'react-router-dom';
+import Register from '../pages/Register';
 
+const router = createBrowserRouter([
+	{
+		path: '/',
+		element: <div>HOME PAGE</div>,
+		children: [
+			{
+				path: 'register',
+				element: <Register />,
+			},
+			{
+				path: 'login',
+				element: <div>LOGIN</div>,
+			},
+		],
+	},
+]);
 
+export default router;
