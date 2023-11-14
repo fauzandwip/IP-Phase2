a3a8367433fefd0367c1857ec551b337681988cc
fauzandwip <fauzan27dp@gmail.com>
2023-11-14 17:18:58 +0700

    server: setup server and routes

---
+++ server/app.js
@@ -0,0 +1,16 @@
+'use strict';
+
+if (process.env.NODE_ENV !== 'production') {
+	require('dotenv').config();
+}
+
+const express = require('express');
+const cors = require('cors');
+
+const app = express();
+
+app.use(cors());
+app.use(express.json());
+app.use(require('./routes'));
+
+module.exports = app;
