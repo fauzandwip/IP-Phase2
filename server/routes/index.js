a9c51dfe32d75d0507cd4eb1111bb7dc2ffa8a5e
fauzandwip <fauzan27dp@gmail.com>
2023-11-14 18:32:05 +0700

    server: create and impelement authentication

--- server/routes/index.js
+++ server/routes/index.js
@@ -1,7 +1,8 @@
 const express = require('express');
+const { authentication } = require('../middlewares');
 const router = express.Router();
 
 router.use('/auth', require('./auth'));
-router.get('/', (req, res) => res.send('TEST'));
+router.get('/', authentication, (req, res) => res.send('TEST'));
 
 module.exports = router;
