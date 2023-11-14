502867ee98782903b8c67ab3898163349762dd74
fauzandwip <fauzan27dp@gmail.com>
2023-11-14 21:41:55 +0700

    client: refactor register/login form into component create axios instance configure register and login to server

--- client/tailwind.config.js
+++ client/tailwind.config.js
@@ -2,7 +2,14 @@
 export default {
 	content: ['./index.html', './src/**/*.{js,ts,jsx,tsx}'],
 	theme: {
-		extend: {},
+		extend: {
+			colors: {
+				'blue-primary': '#4c95ff',
+				'blue-sec': '#eaf3ff',
+				'white-primary': '#f8faff',
+				'white-sec': '#eff4fb',
+			},
+		},
 	},
 	plugins: [],
 };
