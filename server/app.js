4f3985c162dd191d3bb0c3afc69228cd8a4c1d0f
fauzandwip <fauzan27dp@gmail.com>
2023-11-17 08:44:13 +0700

    fix: fix cors

--- server/app.js
+++ server/app.js
@@ -12,15 +12,17 @@ const app = express();
 
 // app.use(cors());
 app.use((req, res, next) => {
-	res.header(
-		'Access-Control-Allow-Origin',
-		'https://iproject-p2-93bd0.web.app'
-	);
-	// res.header('Access-Control-Allow-Origin', 'http://localhost:5173');
+	// res.header(
+	// 	'Access-Control-Allow-Origin',
+	// 	'https://iproject-p2-93bd0.web.app'
+	// );
+	res.header('Access-Control-Allow-Origin', 'http://localhost:5173');
 	res.header('Access-Control-Allow-Credentials', true);
 	res.header('Access-Control-Allow-Methods', 'GET, POST, PUT, PATCH, DELETE');
-	res.header('Access-Control-Allow-Headers', 'Content-Type, Authorization');
-	res.header('Access-Control-Allow-Headers', 'Content-Type, google_token');
+	res.header(
+		'Access-Control-Allow-Headers',
+		'Content-Type, Authorization, google_token'
+	);
 	next();
 });
 app.use(express.json());
