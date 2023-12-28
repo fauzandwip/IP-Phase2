3f67ffbb2a838f28d2bee2bfbda3cc9485fbf245
fauzandwip <fauzan27dp@gmail.com>
2023-12-28 19:53:39 +0700

    server: update server url

--- client/src/api/index.js
+++ client/src/api/index.js
@@ -2,7 +2,7 @@ import axios from 'axios';
 
 const api = axios.create({
 	// baseURL: 'http://localhost:3000',
-	baseURL: 'https://server-p2.fauzandp.online',
+	baseURL: 'https://server-p2-iP.fauzandp.online',
 });
 
 export default api;
